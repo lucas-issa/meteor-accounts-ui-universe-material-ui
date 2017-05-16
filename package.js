d1352c953af96d32d0fddd9a26cc9d5c66d71a42
Lucas Villela Neder Issa <issa@dcc.ufmg.br>
2017-05-16 14:43:03 -0300

    Incrementing version.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'lucasissa:meteor-accounts-ui-universe-material-ui',
-    version: '0.5.0',
+    version: '0.5.1',
     summary: 'Accounts UI replacement for Universe using React and Material UI',
     documentation: 'README.md',
     git: 'https://github.com/lucas-issa/meteor-accounts-ui-universe-material-ui'
