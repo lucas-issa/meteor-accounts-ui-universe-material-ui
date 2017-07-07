9e60504b059f75dffc2dfcb1bf05c374b994d86d
Wesley Simõeswessimoes <wessimoes@synergia.cdd.ufmg.br>
2017-07-07 13:13:15 -0300

    adição do background image na tela loggedIn

--- components/LoggedIn.jsx
+++ components/LoggedIn.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import i18n from 'meteor/universe:i18n';
 import RaisedButton from 'material-ui/RaisedButton';
-import {marginStyle, buttonMarginStyle} from './Styles';
-import {AccountsUiConfig} from '../AccountsUiConfig';
+import { buttonMarginStyle, divExterna, divInterna, marginButtonCard, background } from './Styles';
+import { AccountsUiConfig } from '../AccountsUiConfig';
+import { Card, CardTitle } from 'material-ui/Card';
 //instance of translate component in "accounts-ui" namespace
 const T = i18n.createComponent(i18n.createTranslator('accounts-ui'));
-import { loadingComponent, divExterna, cardActionsStyle, divInterna, marginButtonCard, paddingLeftandRightCard } from './Styles';
-import { Card, CardTitle, CardActions } from 'material-ui/Card';
 
 export default React.createClass({
 
@@ -14,30 +13,34 @@ export default React.createClass({
 
     render () {
         return (
-        <div style={divExterna}>
-            <div style={divInterna}>
-                <Card style={marginButtonCard}>
-                    <CardTitle title={<T>youre_logged_in</T>} subtitle="Faça login para ter acesso ao sistema"/>
+            <div style={background}>
+                <div style={{height: "100vh"}}>
+                    <div style={divExterna}>
+                        <div style={divInterna}>
+                            <Card style={marginButtonCard}>
+                                <CardTitle title={<T>youre_logged_in</T>}/>
 
-                    <div style={marginStyle}>
+                                <div style={{marginLeft: 65, paddingBottom: 15}}>
 
-                        <RaisedButton
-                            style={buttonMarginStyle}
-                            onClick={AccountsUiConfig.onGoToLoggedInHome}
-                            primary={true}
-                            label={<T>back</T>}
-                        />
+                                    <RaisedButton
+                                        style={buttonMarginStyle}
+                                        onClick={AccountsUiConfig.onGoToLoggedInHome}
+                                        primary={true}
+                                        label={<T>back</T>}
+                                    />
 
-                        <RaisedButton
-                            style={buttonMarginStyle}
-                            onClick={() => Meteor.logout()}
-                            label={<T>click_to_log_out</T>}
-                        />
+                                    <RaisedButton
+                                        style={buttonMarginStyle}
+                                        onClick={() => Meteor.logout()}
+                                        label={<T>click_to_log_out</T>}
+                                    />
 
+                                </div>
+                            </Card>
+                        </div>
                     </div>
-                </Card>
+                </div>
             </div>
-        </div>
         );
     }
 });
